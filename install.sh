fecdea9d4736389742e27b8c141ef2065fdde9cb
Jacob D. Moorman <jdmoorman@math.ucla.edu>
2021-04-19 13:26:40 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -88,4 +88,14 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 # zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
+# p10k font
+mkdir font-files
+cd font-files
+curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -OL
+open MesloLGS%20NF%20Regular.ttf
+curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf -OL
+open MesloLGS%20NF%20Bold.ttf
+curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf -LO
+open MesloLGS%20NF%20Italic.ttf
+curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf -OL
+open MesloLGS%20NF%20Bold%20Italic.ttf
