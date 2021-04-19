eb49ed015b7d7b8bada4bf26fcd474d29e6d2130
Jacob D. Moorman <jdmoorman@math.ucla.edu>
2021-04-19 10:41:25 -0700

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -20,17 +20,6 @@ ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
 brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
-# Install a modern version of Bash.
-brew install bash
-brew install bash-completion2
-
-# Switch to using brew-installed bash as default shell
-if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
-  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
-  chsh -s "${BREW_PREFIX}/bin/bash";
-fi;
 
 # Install `wget` with IRI support.
 brew install wget --with-iri
@@ -39,7 +28,6 @@ brew install wget --with-iri
 brew install gnupg
 
 # Install more recent versions of some macOS tools.
-brew install vim --with-override-system-vi
 brew install grep
 brew install openssh
 brew install screen
@@ -52,50 +40,26 @@ brew install sfnt2woff
 brew install sfnt2woff-zopfli
 brew install woff2
 
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
-
 # Install other useful binaries.
-brew install ack
-#brew install exiv2
 brew install git
 brew install git-lfs
 brew install gs
-brew install imagemagick --with-webp
 brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rlwrap
 brew install ssh-copy-id
 brew install tree
-brew install vbindiff
-brew install zopfli
+
+# Casks
+brew install --cask slack
+brew install --cask zoom
+brew install --cask google-chrome
+brew install --cask atom
+brew install --cask timing
+brew install --cask iterm2
+brew install --cask spotify
+brew install --cask zotero
+brew install --cask mactex-no-gui
 
 # Remove outdated versions from the cellar.
 brew cleanup
+
+
